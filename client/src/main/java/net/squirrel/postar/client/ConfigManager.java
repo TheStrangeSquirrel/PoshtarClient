c874f26d34fd48035fd2b51ffc1623ae38bbe085
Malyshev Vladislav <v.malyshev.v@gmail.com>
2016-06-04 15:12:58 +0300

    add property

---
+++ client/src/main/java/net/squirrel/postar/client/ConfigManager.java
@@ -0,0 +1,44 @@
+package net.squirrel.postar.client;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/*
+Load properties from "/config.properties file".
+ */
+public class ConfigManager {
+    private Properties property;
+    private String baseUrl;
+    private String providersUrl;
+    private String trackingUrl;
+
+    public ConfigManager() {
+        this.property = new Properties();
+        try {
+            property.load(new FileInputStream(new File("/config.properties")));
+            loadFields();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadFields() {
+        baseUrl = property.getProperty("BASE_URL");
+        providersUrl = property.getProperty("PROVIDERS_URL");
+        trackingUrl = property.getProperty("TRACKING_URL");
+    }
+
+    public String getTrackingUrl() {
+        return trackingUrl;
+    }
+
+    public String getProvidersUrl() {
+        return providersUrl;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+}
