7b45b10950ab6983f7ece97a1ae63877ddc68274
Malyshev Vladislav <v.malyshev.v@gmail.com>
2016-06-15 18:40:37 +0300

    Added call list providers

--- client/src/main/java/net/squirrel/postar/client/HelloActivity.java
+++ client/src/main/java/net/squirrel/postar/client/HelloActivity.java
@@ -2,24 +2,30 @@ package net.squirrel.postar.client;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+//TODO: РЕализовать статус соединения; блок кнопки newTrack
 public class HelloActivity extends Activity implements View.OnClickListener {
-    private IntentFilter intentFilter = new IntentFilter();
     private Intent intent;
     private Button btnNewTrack, btnSavedTrack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+        findViews();
+        setListeners();
+    }
 
-
+    private void findViews() {
         btnSavedTrack = (Button) findViewById(R.id.newTrack);
         btnNewTrack = (Button) findViewById(R.id.savedTrack);
+    }
+
+    private void setListeners() {
         btnSavedTrack.setOnClickListener(this);
         btnNewTrack.setOnClickListener(this);
     }
@@ -28,12 +34,11 @@ public class HelloActivity extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.newTrack:
-                intent = new Intent(this, AllPost.class);
+                intent = new Intent(this, ProvidersActivity.class);
                 startActivity(intent);
                 break;
             case R.id.savedTrack:
                 break;
         }
-
     }
 }
